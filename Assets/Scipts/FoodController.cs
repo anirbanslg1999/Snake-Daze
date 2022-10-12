53de360cbb82f478fea64342f5413d424a854366
Anirban Chakraborty <anirbanslg1999@gmail.com>
2022-10-12 22:29:31 +0530

    Added FoodSystem

---
+++ Assets/Scipts/FoodController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FoodController : MonoBehaviour
+{
+    [SerializeField] BoxCollider2D confiner;
+    [SerializeField] List<Sprite> foodSprites = new List<Sprite>();
+    private SpriteRenderer foodSpriteRenderer;
+
+    private int randNumForSpriteSelection;
+    
+    FoodType foodType;
+    private void Awake()
+    {
+        foodSpriteRenderer = GetComponent<SpriteRenderer>();
+        FoodPositionAllocation();
+    }
+
+    private FoodType DeterminingTheFoodType()
+    {
+        randNumForSpriteSelection = Random.Range(0, 100);
+        if(randNumForSpriteSelection < 65)
+        {
+            foodType = FoodType.Healthy;
+        }
+        else
+        {
+            foodType = FoodType.SpecialFood;
+        }
+        return foodType;
+    }
+    private void FoodPositionAllocation()
+    {
+        foodType = DeterminingTheFoodType();
+        if (foodType == FoodType.Healthy)
+        {
+            foodSpriteRenderer.sprite = foodSprites[0];
+            // healthy feature...
+        }
+        else if(foodType == FoodType.SpecialFood)
+        {
+            foodSpriteRenderer.sprite = foodSprites[1];
+            // posion features...
+        }
+        Bounds bounds = confiner.bounds;
+        float tempX = Random.Range(bounds.min.x, bounds.max.x);
+        float tempY = Random.Range(bounds.min.y, bounds.max.y);
+        this.transform.position = new Vector3(Mathf.Round(tempX), Mathf.Round(tempY), 0);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<SnakeController>() != null)
+        {
+            FoodPositionAllocation();
+        }
+    }
+
+
+
+}
+public enum FoodType
+{
+    Healthy,
+    SpecialFood
+}
--- Assets/Scipts/SnakeController.cs
+++ Assets/Scipts/SnakeController.cs
@@ -1,20 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SnakeController : MonoBehaviour
 {
+    //Using enums to define the Character number to not make different scrpits for it
     [SerializeField] characterTypes character;
     private Vector2 direction;
     private Vector2 savedDirection;
 
+    // Accessing the player controller script by name a object in the awake methord
     private PlayerController playerController;
+    // To store the input action inside it.
     InputAction move;
     private void Awake()
     {
+        // making the object of class player controller
         playerController = new PlayerController();
+        //checking on which input type the playercontroller should be passed. 
         if(character == characterTypes.player1)
         {
         move = playerController.Snake1.Move;
@@ -23,24 +25,22 @@ public class SnakeController : MonoBehaviour
         {
         move = playerController.Snake2.Move;
         }
+        // subscribing to the event 
         move.performed += OnMove;
         move.Enable();
-    }
-    private void Start()
-    {
-        savedDirection = direction;
-
     }
     void Update()
     {
         SetDirection();
     }
-    public void OnMove(InputAction.CallbackContext context)
+    private void OnMove(InputAction.CallbackContext context)
     {
+        // Storing the input. 
         direction = context.ReadValue<Vector2>();
     }
     private void SetDirection()
     {
+        // Defining the direction to go and where not to go... like cant go in the opposide direction.
         if (direction.x != 0 && direction.y != 0)
         {
             direction = new Vector2(savedDirection.x, savedDirection.y);
@@ -57,7 +57,9 @@ public class SnakeController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // changing the transform of the player to move.
         this.transform.position = new Vector3(Mathf.Round(transform.position.x) + direction.x, Mathf.Round(transform.position.y) + direction.y, 0);
+        // storig the value to reuse it again.
         savedDirection = direction;
     }
 
